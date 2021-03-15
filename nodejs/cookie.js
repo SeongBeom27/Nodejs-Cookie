453c4028a21b10437ce860d4088263249d4a9e1f
SeongBeom27 <lgh121546@naver.com>
2021-03-15 19:54:14 +0900

    Node, Cookie 3. 쿠키 읽기

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -1,8 +1,15 @@
 var http = require('http');
+var cookie = require('cookie');
+
 http.createServer(function(request, response) {
-    // writeHead 두번째 인자에 쿠키를 객체 형식으로 넣어준다.
-    // response.writeHead(200, {
-    //     'Set-Cookie': ['yummy_cookie=choco', 'tasty_cookie=strawberry']
-    // });
+    var cookies = {};
+    // parse는 undefined를 처리 못한다.
+    if (request.headers.cookie != undefined) {
+        cookies = cookie.parse(request.headers.cookie);
+    }
+    console.log(cookies.yummy_cookie);
+    response.writeHead(200, {
+        'Set-Cookie': ['yummy_cookie=choco', 'tasty_cookie=strawberry']
+    });
     response.end('Cookie!!');
 }).listen(3000);
