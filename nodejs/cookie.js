e175c7130b141416bef9dd3b2ee1112f8c95d22d
SeongBeom27 <lgh121546@naver.com>
2021-03-15 20:07:36 +0900

    Node, Cookie 4. 쿠키의 종류 (Permanent, Session)

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -8,8 +8,12 @@ http.createServer(function(request, response) {
         cookies = cookie.parse(request.headers.cookie);
     }
     console.log(cookies.yummy_cookie);
+    // Max-Age는 쿠키가 얼마동안 살 것인가.
     response.writeHead(200, {
-        'Set-Cookie': ['yummy_cookie=choco', 'tasty_cookie=strawberry']
+        'Set-Cookie': ['yummy_cookie=choco',
+            'tasty_cookie=strawberry',
+            `Permanent=cookies; Max-Age=${60*60*24*30}`
+        ]
     });
     response.end('Cookie!!');
 }).listen(3000);
